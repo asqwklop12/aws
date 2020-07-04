47b3b1d4c40203c8078f694b50cc473e2726e102
klom <asqwklop12@gmail.com>
2020-07-05 00:29:22 +0900

    HelloController에 DTO URL 추가

--- src/test/java/com/klom/springboot/web/HelloControllerTest.java
+++ src/test/java/com/klom/springboot/web/HelloControllerTest.java
@@ -2,14 +2,17 @@ package com.klom.springboot.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @RunWith(SpringRunner.class)
@@ -25,5 +28,19 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
            .andExpect(status().isOk())
            .andExpect(content().string("hello"));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                   .param("name",name)
+                   .param("amount",String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount",is(amount)));
+
     }
 }
