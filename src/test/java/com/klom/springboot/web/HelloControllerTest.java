fbeea75575eacdc5bd7650ddb1b8b88ed79ccada
klom <asqwklop12@gmail.com>
2020-07-04 23:47:07 +0900

    feat: HelloController 생성

---
+++ src/test/java/com/klom/springboot/web/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package com.klom.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+        mvc.perform(get("/hello"))
+           .andExpect(status().isOk())
+           .andExpect(content().string("hello"));
+    }
+}
