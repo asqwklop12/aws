166c8d65122caca373f42e203a81439ec9f19f00
klom <asqwklop12@gmail.com>
2020-07-05 08:30:29 +0900

    전체글 보이기..

--- src/main/java/com/klom/springboot/service/PostsService.java
+++ src/main/java/com/klom/springboot/service/PostsService.java
@@ -5,10 +5,14 @@ import com.klom.springboot.domain.posts.Posts;
 import com.klom.springboot.web.dto.PostResponseDto;
 import com.klom.springboot.web.dto.PostSaveRequestDto;
 import com.klom.springboot.web.dto.PostUpdateRequestDto;
+import com.klom.springboot.web.dto.PostsListResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @RequiredArgsConstructor
 @Service
@@ -35,7 +39,12 @@ public class PostsService {
     public PostResponseDto findById(Long id) {
         Posts entity = postRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id = " + id));
-
         return new PostResponseDto(entity);
+    }
+
+    public List<PostsListResponseDto> findAllDesc() {
+        return postRepository.findAllDesc().stream()
+                            .map(PostsListResponseDto::new)
+                            .collect(toList());
     }
 }
